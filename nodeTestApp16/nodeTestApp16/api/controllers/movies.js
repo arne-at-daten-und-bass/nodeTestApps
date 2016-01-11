72c8b02d86f6b7ae257e2ee8388edc0a6e4dcfd1
arnoldDB <ich@BigMac.fritz.box>
2016-01-11 05:56:52 +0100

    Refactored to one 'cypherRequest' function,  some validation and error checking still necessary.

--- nodeTestApp16/nodeTestApp16/api/controllers/movies.js
+++ nodeTestApp16/nodeTestApp16/api/controllers/movies.js
@@ -2,150 +2,110 @@
 
 var request = require('request');
 
-var host = '172.17.0.4';
+var host = '172.17.0.5';
 var port = 7474;
-
-
-//var dbRESTUrl ='http://neo4j:password@' + host + ':' + port + '/db/data/transaction/commit';
 var dbRESTUrl ='http://' + host + ':' + port + '/db/data/transaction/commit';
+
+var includeStats = false;
+
+function cypherRequest(query, params, includeStats, callback) {
+  request.post({
+    headers: {
+      'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
+      'Content-Type': 'application/json',
+      'Accept': 'application/json; charset=UTF-8'},
+    uri: dbRESTUrl,
+    json: {statements: [{statement: query, parameters: params, includeStats: includeStats}]}},
+    function(err, res) {
+      callback(err, res.body);
+    }
+  );
+}
 
 module.exports = {
 
   index: function(req, res) {
-    var query = 'MATCH (n:Movie) RETURN n LIMIT 10';
-    var dataFromNeo4j = [];
-    var dataToSwagger = [];
+    var query = 'MATCH (m:Movie {released: {released}}) RETURN m';
+    var params = {
+      'released': parseInt(req.swagger.params.released.value)
+    };
+    if(req.swagger.params.released.value === undefined) {
+      query = 'MATCH (m:Movie) RETURN m';
+    }  
 
-    request.post({
-      headers: {
-        'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json; charset=UTF-8'
-      },
-      uri: dbRESTUrl,
-      json: {statements: [{statement: query}]}
-    }, function (error, response) {
-        dataFromNeo4j = response.body.results[0].data;
-        dataFromNeo4j.map(function(element, index) {
-          dataToSwagger[index] = element.row;
-        });
-        res.json({
-          movies: dataToSwagger
-        });
-    });
-  },
+    function callback(error, responseBody) {
+      var dataFromNeo4j = [];
+      var dataToSwagger = [];
+      dataFromNeo4j = responseBody.results[0].data;
+      dataFromNeo4j.map(function(element, index) {
+        dataToSwagger[index] = element.row;
+      });
+      res.json({
+        movies: dataToSwagger
+      });
+    }
 
-  // readOne: function(req, res) {
-  //   var released = req.swagger.params.released.value;
-  //   var query = 'MATCH (m:Movie {released: "' + released + '"}) RETURN m';
-  //   console.log(query);
-  //   var dataFromNeo4j = [];
-  //   var dataToSwagger = [];
-
-  //     request.post({
-  //       headers: {
-  //         'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json; charset=UTF-8'
-  //       },
-  //       uri: dbRESTUrl,
-  //       json: {statements: [{statement: query}]}
-  //     }, function (error, response) {
-  //         dataFromNeo4j = response.body.results[0].data;
-  //         dataFromNeo4j.map(function(element, index) {
-  //           dataToSwagger[index] = element.row;
-  //         });
-  //         res.json({
-  //           movies: dataToSwagger
-  //         });
-  //     });
-  //   },    
+    cypherRequest(query, params, includeStats, callback);
+  },  
 
   create: function(req, res) {
     var query = 'CREATE (m:Movie { title: {title}, released: {released}, tagline: {tagline} }) RETURN m'; 
     var params = req.swagger.params.movie.value.movie;
-
-    request.post({
-      headers: {
-        'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json; charset=UTF-8'
-      },
-      uri: dbRESTUrl,
-      json: {statements: [{statement: query, parameters: params}]}
-    }, function (error, response) {
+    function callback(error, responseBody) {
         res.json({
-          movie: params
+          movie: responseBody.results[0].data 
         });
-    });   
+    }
+
+    cypherRequest(query, params, includeStats, callback);   
   },
 
   show: function(req, res) {
-    var id = req.swagger.params.movieId.value;
-    var query = 'MATCH m WHERE id(m)=' + id + ' RETURN m';
-    console.log(query);
-    var dataFromNeo4j = [];
-    var dataToSwagger = [];
+    var query = 'MATCH m WHERE id(m)={id} RETURN m';
+    var params = {
+      'id': parseInt(req.swagger.params.movieId.value)
+    }; 
 
-    request.post({
-      headers: {
-        'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json; charset=UTF-8'
-      },
-      uri: dbRESTUrl,
-      json: {statements: [{statement: query}]}
-    }, function (error, response) {
-        console.log(response.body.results[0].data[0].row[0]);
-        res.json({
-          movie: response.body.results[0].data[0].row[0]
-        });
-    });
+    function callback(error, responseBody){
+      res.json({
+        movie: responseBody.results[0].data
+      });
+    }
+
+    cypherRequest(query, params, includeStats, callback);
   },
 
   update: function(req, res) {
+    var query = 'MATCH m WHERE id(m)={id} SET m={props} RETURN m';
     var params = {
       'id': parseInt(req.swagger.params.movieId.value),
       'props': req.swagger.params.movie.value.movie
       }; 
-    console.log(params);
-    var query = 'MATCH m WHERE id(m)={id} SET m={props} RETURN m';
-    console.log(query);
-    request.post({
-      headers: {
-        'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json; charset=UTF-8'
-      },
-      uri: dbRESTUrl,
-      json: {statements: [{statement: query, parameters: params}]}
-    }, function (error, response) {
-        res.json({
-          movie: params.props
-        });
-    });   
+
+    function callback(error, responseBody) {
+      res.json({
+        movie: responseBody.results[0].data
+      });
+    }   
+
+    cypherRequest(query, params, includeStats, callback);
   },
 
   delete: function(req, res) {
+    var query = 'MATCH m WHERE id(m)={id} DELETE m';
     var params = {
       'id': parseInt(req.swagger.params.movieId.value)
     }; 
-    console.log(params);
-    var query = 'MATCH m WHERE id(m)={id} DELETE m';
-    console.log(query);
-    request.post({
-      headers: {
-        'Authorization': 'Basic bmVvNGo6cGFzc3dvcmQ=',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json; charset=UTF-8'
-      },
-      uri: dbRESTUrl,
-      json: {statements: [{statement: query, parameters: params}]}
-    }, function (error, response) {
-        res.json({
-          movie: { "title": "dummy", "released": 2000, "tagline": "dummy"}
-        });
-    });   
+    includeStats = true;
+
+    function callback(error, responseBody) {
+      console.log(JSON.stringify(responseBody));
+      res.json({
+        nodes_deleted: responseBody.results[0].stats.nodes_deleted
+      });
+    }
+
+    cypherRequest(query, params, includeStats, callback);   
   }
 
 };
