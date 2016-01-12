007ea3f8fa05a41e329eb3082c588a0ea4b987c1
arnoldDB <ich@BigMac.fritz.box>
2016-01-12 18:01:56 +0100

    Initial config files (still unused)

---
+++ nodeTestApp16/nodeTestApp16/config/config.global.js
@@ -0,0 +1,20 @@
+var config = module.exports = {};
+
+//general
+config.environment = {};
+config.environment.directory = ('../../../nodeTestAppsEnvironments/nodeApp16Env');
+
+//nodeJS and express
+config.web = {};
+config.web.http = {};
+config.web.https = {};
+
+//neo4j
+config.db = {};
+config.db.http = {};
+config.db.https = {};
+
+//popoto
+config.pop = {}
+
+//client
