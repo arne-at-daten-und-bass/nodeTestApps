9794e741e4961d51b122aa8a9f95d4c279658d52
arnoldDB <ich@BigMac.fritz.box>
2016-02-11 14:36:48 +0100

    Same as before, but now all old code removed

--- nodeTestApp16/nodeTestApp16/api/controllers/html_web_movies.js
+++ nodeTestApp16/nodeTestApp16/api/controllers/html_web_movies.js
@@ -9,12 +9,6 @@ var resultType = ["row"];
 // HTML Web:
 module.exports = {
 
-  // index: function(req, res) {
-  //   res.render('index', 
-  //     { slogan: 'Movie World' 
-  //   });  
-  // },
-
   getCreate: function(req, res) {
     res.render('movies/create', 
       { slogan: 'Create a new Movie' 
@@ -24,158 +18,58 @@ module.exports = {
   create: function(req, res) {
     var query = neo.queries.movies().create();
     var params = neo.params.movies().create(req.swagger.params.title.value, req.swagger.params.released.value, req.swagger.params.tagline.value);
-    // {
-    //   'title': req.swagger.params.title.value,
-    //   'released': parseInt(req.swagger.params.released.value),
-    //   'tagline': req.swagger.params.tagline.value
-    // };
-    
     var callback = neo.callbacks.movies.create(res).web;
-    // var callback = neo.callbacks.movies(res).create;
-    // function callback(error, responseBody) {
-    //   res.render('movies/read', {
-    //     slogan: 'New Movie created',
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }
-
-    neo.requests.movies().create(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);   
+    neo.requests.movies().create(query, params, resultType, includeStats, callback); 
   }, 
 
   readBulk: function(req, res) {
     var query = neo.queries.movies().readBulkParam();
-    var params = neo.params.movies().readBulkParam(req.swagger.params.released.value);
-    // var params = {
-    //   'released': parseInt(req.swagger.params.released.value)
-    // };
+
     if(req.swagger.params.released.value === undefined) {
       query = neo.queries.movies().readBulkNoParam();
-    }  
-
+    }
+     
+    var params = neo.params.movies().readBulkParam(req.swagger.params.released.value);
     var callback = neo.callbacks.movies.readBulk(res).web;
-    // var callback = neo.callbacks.movies(res).readBulk;
-    // function callback(error, responseBody) {
-    //   var movies = [];
-    //   movies = neo.helpers.readBulk(error, responseBody);
-
-    //   res.render('movies/readBulk', {
-    //     slogan: 'All The Movies',
-    //     movies: movies
-    //   });
-    // }
-
     neo.requests.movies().readBulk(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   }, 
 
   read: function(req, res) {
     var query = neo.queries.movies().read();
     var params = neo.params.movies().read(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // }; 
-
     var callback = neo.callbacks.movies.read(res).web;
-    // var callback = neo.callbacks.movies(res).read;
-    // function callback(error, responseBody){
-    //   res.render('movies/read', {
-    //     slogan: 'Movie',
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }
-
     neo.requests.movies().read(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   getUpdate: function(req, res) {
     var query = neo.queries.movies().getUpdate();
     var params = neo.params.movies().getUpdate(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // }; 
-    
     var callback = neo.callbacks.movies.getUpdate(res).web;
-    // var callback = neo.callbacks.movies(res).getUpdate;
-    // function callback(error, responseBody){
-    //   res.render('movies/update', { 
-    //     slogan: 'Update a Movie',
-    //     id: parseInt(req.swagger.params.movieId.value),
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }
-
     neo.requests.movies().getUpdate(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   update: function(req, res) {
     var query = neo.queries.movies().update();
     var params = neo.params.movies().update(req.swagger.params.movieId.value, req.swagger.params.title.value, req.swagger.params.released.value, req.swagger.params.tagline.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value),
-    //   'props': {
-    //     'title': req.swagger.params.title.value,
-    //     'released': parseInt(req.swagger.params.released.value),
-    //     'tagline': req.swagger.params.tagline.value
-    //     }
-    //   };
-
-    var callback = neo.callbacks.movies.update(res).web;
-    // var callback = neo.callbacks.movies(res).update;
-    // function callback(error, responseBody) {
-    //   res.render('movies/read', {
-    //     slogan: 'Movie updated',
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }   
-
+    var callback = neo.callbacks.movies.update(res).web; 
     neo.requests.movies().update(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   getDelete: function(req, res) {
     var query = neo.queries.movies().getDelete();
     var params = neo.params.movies().getDelete(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // }; 
-
     var callback = neo.callbacks.movies.getDelete(res).web;
-    // var callback = neo.callbacks.movies(res).getDelete;
-    // function callback(error, responseBody){
-    //   res.render('movies/delete', { 
-    //     slogan: 'Delete a Movie (and all its relationships)',
-    //     id: parseInt(req.swagger.params.movieId.value),
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }
-
     neo.requests.movies().getDelete(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   delete: function(req, res) {
     var query = neo.queries.movies().delete();
     var params = neo.params.movies().delete(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // }; 
-    
+
     includeStats = true;
 
     var callback = neo.callbacks.movies.delete(res).web;
-    // var callback = neo.callbacks.movies(res).delete;
-    // function callback(error, responseBody) {
-    //   res.render('movies/deleted', {
-    //     slogan: 'Amount of deleted movies',
-    //     nodes_deleted: responseBody.results[0].stats.nodes_deleted
-    //   });
-    // }
-
-    neo.requests.movies().delete(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);   
+    neo.requests.movies().delete(query, params, resultType, includeStats, callback);  
   }
 
 };
--- nodeTestApp16/nodeTestApp16/api/controllers/json_api_movies.js
+++ nodeTestApp16/nodeTestApp16/api/controllers/json_api_movies.js
@@ -10,106 +10,45 @@ module.exports = {
 
   create: function(req, res) {
     var query = neo.queries.movies().create();
-    // var query = neo.movies.createQuery();
     var params = neo.params.movies().create(req.swagger.params.movie.value.movie.title, req.swagger.params.movie.value.movie.released, req.swagger.params.movie.value.movie.tagline);
-    // var params = req.swagger.params.movie.title.value;
-
     var callback = neo.callbacks.movies.create(res).api;
-    // var callback = neo.callbacks.movies(res).createViaAPI;
-    // function callback(error, responseBody) {
-    //   res.json({
-    //     movie: responseBody.results[0].data 
-    //   });
-    // }
-
-    neo.requests.movies().create(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);  
+    neo.requests.movies().create(query, params, resultType, includeStats, callback); 
   },
 
   readBulk: function(req, res) {
     var query = neo.queries.movies().readBulkParam();
-    // var query = neo.movies.queries().readBulkParam();
-    var params = neo.params.movies().readBulkParam(req.swagger.params.released.value);
-    // var params = {
-    //   'released': parseInt(req.swagger.params.released.value)
-    // };
+
     if(req.swagger.params.released.value === undefined) {
       query = neo.queries.movies().readBulkNoParam();
-    }  
+    } 
 
+    var params = neo.params.movies().readBulkParam(req.swagger.params.released.value);
     var callback = neo.callbacks.movies.readBulk(res).api;
-    // var callback = neo.callbacks.movies(res).readBulkViaAPI;
-    // function callback(error, responseBody) {
-    //   var movies = [];
-    //   movies = neo.helpers.readBulk(error, responseBody);
-
-    //   res.json({
-    //     movies:  movies
-    //   });
-    // }
-
     neo.requests.movies().readBulk(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   }, 
 
   read: function(req, res) {
     var query = neo.queries.movies().read();
-    // var query = neo.movies.queries().read();
     var params = neo.params.movies().read(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // }; 
-
     var callback = neo.callbacks.movies.read(res).api;
-    // function callback(error, responseBody){
-    //   res.json({
-    //     movie: responseBody.results[0].data[0].row[0]
-    //   });
-    // }
-
     neo.requests.movies().read(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   update: function(req, res) {
     var query = neo.queries.movies().update();
-    // var query = neo.movies.queries().update();
-    var params = neo.params.movies().update(req.swagger.params.movieId.value, req.swagger.params.movie.value.movie.title, req.swagger.params.movie.value.movie.released, req.swagger.params.movie.value.movie.tagline);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value),
-    //   'props': req.swagger.params.movie.value.movie
-    //   }; 
-
+    var params = neo.params.movies().update(req.swagger.params.movieId.value, req.swagger.params.movie.value.movie.title, req.swagger.params.movie.value.movie.released, req.swagger.params.movie.value.movie.tagline); 
     var callback = neo.callbacks.movies.update(res).api;
-    // function callback(error, responseBody) {
-    //   res.json({
-    //     movie: responseBody.results[0].data
-    //   });
-    // }   
-
     neo.requests.movies().update(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   },
 
   delete: function(req, res) {
     var query = neo.queries.movies().delete();
-    // var query = neo.movies.queries().delete();
     var params = neo.params.movies().delete(req.swagger.params.movieId.value);
-    // var params = {
-    //   'id': parseInt(req.swagger.params.movieId.value)
-    // };
-    
+
     includeStats = true;
 
     var callback = neo.callbacks.movies.delete(res).api;
-    // function callback(error, responseBody) {
-    //   res.json({
-    //     nodes_deleted: responseBody.results[0].stats.nodes_deleted
-    //   });
-    // }
-
-    neo.requests.movies().delete(query, params, resultType, includeStats, callback);
-    // neo.cypherRequest(query, params, resultType, includeStats, callback);   
+    neo.requests.movies().delete(query, params, resultType, includeStats, callback);  
   }
 
 };
