e8a6ac586393e9036972f6992b150435c2c5e646
arnoldDB <ich@BigMac.fritz.box>
2016-02-11 14:09:19 +0100

    Rewrote according to new neo.js pattern

--- nodeTestApp16/nodeTestApp16/api/controllers/json_api_movies.js
+++ nodeTestApp16/nodeTestApp16/api/controllers/json_api_movies.js
@@ -9,56 +9,47 @@ var resultType = ["row"];
 module.exports = {
 
   create: function(req, res) {
-    var query = neo.movies.createQuery();
-    var params = req.swagger.params.movie.value.movie;
+    var query = neo.queries.movies().create();
+    // var query = neo.movies.createQuery();
+    var params = neo.params.movies().create(req.swagger.params.movie.value.movie.title, req.swagger.params.movie.value.movie.released, req.swagger.params.movie.value.movie.tagline);
+    // var params = req.swagger.params.movie.title.value;
 
-    function callback(error, responseBody) {
-      res.json({
-        movie: responseBody.results[0].data 
-      });
-    }
+    var callback = neo.callbacks.movies.create(res).api;
+    // var callback = neo.callbacks.movies(res).createViaAPI;
+    // function callback(error, responseBody) {
+    //   res.json({
+    //     movie: responseBody.results[0].data 
+    //   });
+    // }
 
-    neo.cypherRequest(query, params, resultType, includeStats, callback);  
-  },
-
-  graph: function(req, res) {
-    var query = neo.graph.readAllQuery();
-    var params = {};
-    
-    resultType = ["graph"];
-
-    function callback(error, response) {
-      var graph = {};
-      graph = neo.helpers.nodeLinks(error, response) ;
-      
-      res.json({
-        slogan: 'Visual',
-        graph: graph
-      });
-    }
-
-    neo.cypherRequest(query, params, resultType, includeStats, callback);
+    neo.requests.movies().create(query, params, resultType, includeStats, callback);
+    // neo.cypherRequest(query, params, resultType, includeStats, callback);  
   },
 
   readBulk: function(req, res) {
-    var query = neo.movies.queries().readBulkParam();
-    var params = {
-      'released': parseInt(req.swagger.params.released.value)
-    };
+    var query = neo.queries.movies().readBulkParam();
+    // var query = neo.movies.queries().readBulkParam();
+    var params = neo.params.movies().readBulkParam(req.swagger.params.released.value);
+    // var params = {
+    //   'released': parseInt(req.swagger.params.released.value)
+    // };
     if(req.swagger.params.released.value === undefined) {
-      query = neo.movies.queries().readBulkNoParam();
+      query = neo.queries.movies().readBulkNoParam();
     }  
 
-    function callback(error, responseBody) {
-      var movies = [];
-      movies = neo.helpers.readBulk(error, responseBody);
+    var callback = neo.callbacks.movies.readBulk(res).api;
+    // var callback = neo.callbacks.movies(res).readBulkViaAPI;
+    // function callback(error, responseBody) {
+    //   var movies = [];
+    //   movies = neo.helpers.readBulk(error, responseBody);
 
-      res.json({
-        movies:  movies
-      });
-    }
+    //   res.json({
+    //     movies:  movies
+    //   });
+    // }
 
-    neo.cypherRequest(query, params, resultType, includeStats, callback);
+    neo.requests.movies().readBulk(query, params, resultType, includeStats, callback);
+    // neo.cypherRequest(query, params, resultType, includeStats, callback);
   }, 
 
   read: function(req, res) {
