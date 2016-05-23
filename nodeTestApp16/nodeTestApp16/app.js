6c2b502eadb9d6d6fe74836395f12bc69531eb7f
arnoldDB <ich@BigMac.fritz.box>
2016-05-23 08:23:24 +0200

    Now only path for urls (instead of string concat)

--- nodeTestApp16/nodeTestApp16/app.js
+++ nodeTestApp16/nodeTestApp16/app.js
@@ -11,7 +11,7 @@ var options = {key: process.env.WEB_HTTPS_KEY, cert: process.env.WEB_HTTPS_CRT};
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
-module.exports = app; // for testing
+// module.exports = app; // for testing
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +23,9 @@ app.use(forceSSL);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/en', express.static(path.join(__dirname, 'public')));
-app.use('/es', express.static(__dirname + '/public')); //@TODO: Harmonize path or '+'
-app.use('/de', express.static(__dirname + '/public'));
-app.use('/fr', express.static(__dirname + '/public'));
+app.use('/es', express.static(path.join(__dirname, 'public')));
+app.use('/de', express.static(path.join(__dirname, 'public')));
+app.use('/fr', express.static(path.join(__dirname, 'public')));
 
 var config = {
   appRoot: __dirname // required config
@@ -35,6 +35,11 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   // install middleware
+  app.use(function (req, res, next) {
+	console.log('Time: %d', Date.now());
+	next();
+  });
+
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
@@ -49,4 +54,4 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
 var httpsServer = https.createServer({key: process.env.WEB_HTTPS_KEY, cert: process.env.WEB_HTTPS_CRT}, app);
 httpsServer.listen(app_config.web().https.port);
 
-// module.exports = app; 
+module.exports = app; 
